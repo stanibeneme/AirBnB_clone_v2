1f34354e7b71ac2d6ec8fedf36e1ac76b5f8cf79
Stanley Ibeneme <stanleyibeneme92@gmail.com>
2024-01-11 16:45:56 -0800

    IP

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@
 from fabric.api import *
 from datetime import datetime
 from os import path
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['54.160.75.155', '54.89.116.234']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 def do_deploy(archive_path):
